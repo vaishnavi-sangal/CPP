078285dc8449c7985c5dc0e28f40b88f3ece69ee
Vaishnavi Sangal <89699096+vaishnavi-sangal@users.noreply.github.com>
2022-07-26 00:38:57 +0530

    Time: 13 ms (63.11%), Space: 9 MB (82.84%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -1,42 +1,27 @@
-class Solution {
+
+   class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        vector<int>hash1(26,0);
-        vector<int>hash2(26,0);
-        for(auto ch :words[0])
-        {
-            hash1[ch-'a']++;
-        }
-        for(int i=1;i<words.size();i++)
-        {
-            for(auto ch :words[i])
-            {
-                hash2[ch-'a']++;
-            }
-            for(int i=0;i<26;i++)
-            {
-                hash1[i]=min(hash1[i],hash2[i]);
-                hash2[i]=0;
-            }
-                
-        }
-        vector<string>ans;
-        for(int i=0;i<26;i++)
-        {
-            if(hash1[i]>0)
-            {
-                int count = hash1[i];
-                while(count--)
-				{
-					char x = i+ 'a';
-					string s ;
-					s = x;
-					ans.push_back(s);
-
-				}
+        vector<string> v;
+        string first=words[0];
+        for(char x:first){
+            string c="";
+            c+=x;
+            bool f=false;
+            for(int i=1;i<words.size();i++){
+                auto found=words[i].find(c);
+                if(found!=-1)
+                {f=true; 
+                 words[i][found]='3';}
+                else
+                { f=false; break;}
                 
             }
+            if(f==true){
+                v.push_back(c);
         }
-        return ans;
+        }
+        return v;
+  
     }
 };
