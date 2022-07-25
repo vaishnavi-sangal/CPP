5446b4412628101af7335bcad2b4c268f6a7b754
Vaishnavi Sangal <89699096+vaishnavi-sangal@users.noreply.github.com>
2022-07-26 00:22:26 +0530

    Time: 12 ms (67.65%), Space: 9.1 MB (82.84%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int>hash1(26,0);
+        vector<int>hash2(26,0);
+        for(auto ch :words[0])
+        {
+            hash1[ch-'a']++;
+        }
+        for(int i=1;i<words.size();i++)
+        {
+            for(auto ch :words[i])
+            {
+                hash2[ch-'a']++;
+            }
+            for(int i=0;i<26;i++)
+            {
+                hash1[i]=min(hash1[i],hash2[i]);
+                hash2[i]=0;
+            }
+                
+        }
+        vector<string>ans;
+        for(int i=0;i<26;i++)
+        {
+            if(hash1[i]>0)
+            {
+                int count = hash1[i];
+                while(count--)
+				{
+					char x = i+ 'a';
+					string s ;
+					s = x;
+					ans.push_back(s);
+
+				}
+                
+            }
+        }
+        return ans;
+    }
+};
