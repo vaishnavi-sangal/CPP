7da149df778d914e6cea1d32da73f40a3ed1456d
Vaishnavi Sangal <89699096+vaishnavi-sangal@users.noreply.github.com>
2022-08-28 14:24:43 +0530

    Time: 26 ms (74.06%), Space: 26.4 MB (43.54%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,22 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int rootIdx = 0;
-        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
-    }
+
+TreeNode* func(vector<int>& inorder,int i1,int j1,vector<int>& preorder,int i2,int j2,map<int,int> &m)
+{
+    if(i1>j1 || i2>j2)return NULL;
+    TreeNode *node=new TreeNode(preorder[i2]);
     
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right) {
-        if (left > right) return NULL;
-        int pivot = left;  // find the root from inorder
-        while(inorder[pivot] != preorder[rootIdx]) pivot++;
-        
-        rootIdx++;
-        TreeNode* newNode = new TreeNode(inorder[pivot]);
-        newNode->left = build(preorder, inorder, rootIdx, left, pivot-1);
-        newNode->right = build(preorder, inorder, rootIdx, pivot+1, right);
-        return newNode;
-    }
+    int ind=m[preorder[i2]];
+    int numofele=ind-i1;
+    
+    node->left=func(inorder,i1,ind-1,preorder,i2+1,i2+numofele,m);
+    node->right=func(inorder,ind+1,j1,preorder,i2+numofele+1,j2,m);
+    
+    return node;
+}
+
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) 
+{
+    map<int,int>m;
+    for(int i=0;i<inorder.size();i++)m[inorder[i]] = i;
+    
+    return func(inorder,0,inorder.size()-1,preorder,0,preorder.size()-1,m);
+}
 };
