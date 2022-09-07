648d650af585d45d81a3ac9f3004931753f3057a
Vaishnavi Sangal <89699096+vaishnavi-sangal@users.noreply.github.com>
2022-09-07 22:26:25 +0530

    Time: 158 ms (34.77%), Space: 34.3 MB (99.13%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int>hash(26,0);
+        for(char c:tasks)hash[c-'A']++;
+        sort(hash.begin(),hash.end(),greater<int>());
+        int slots=hash[0]-1;
+        int idle=slots*n;
+        for(int i=1;i<hash.size();i++)
+        {
+            idle-=min(slots,hash[i]);
+        }
+        return tasks.size()+max(0,idle);
+    }
+};
