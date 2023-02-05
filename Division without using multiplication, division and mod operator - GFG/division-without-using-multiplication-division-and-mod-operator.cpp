ab246856eb8b0f8e09df972f8ae4d2d6fe2dd60e
Vaishnavi Sangal <89699096+vaishnavi-sangal@users.noreply.github.com>
2023-02-05 20:47:18 +0530

    Added solution - LeetHub

--- Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.cpp
+++ Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.cpp
@@ -14,8 +14,18 @@ class Solution
     public:
     long long divide(long long dividend, long long divisor) 
     {
-        return dividend/divisor;
-        //code here
+        long long quotient = 0;
+        long long temp = 0;
+        long long sign = (dividend >= 0) ^ (divisor >= 0) ? -1 : 1;
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+        for(int i = 31; i >= 0; i--){
+            if(temp + (divisor << i) <= dividend){
+                temp += divisor << i;
+                quotient += (1LL << i);
+            }
+        }
+        return quotient*sign;
     }
 };
 
