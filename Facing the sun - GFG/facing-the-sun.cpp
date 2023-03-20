4b0c52adf5607f6c87919d3b4c172eb6b4724d03
Vaishnavi Sangal <89699096+vaishnavi-sangal@users.noreply.github.com>
2023-03-20 16:03:56 +0530

    Added solution - LeetHub

---
+++ Facing the sun - GFG/facing-the-sun.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Returns count buildings that can see sunlight
+	int countBuildings(int h[], int n) {
+	    int left[n],c=0;
+	    left[0]=h[0];
+	    for(int i=1;i<n;i++)
+	    {
+	        left[i]=max(h[i],left[i-1]);
+	    }
+	    for(int i=1;i<n;i++)
+	    {
+	        if(h[i]>left[i-1])
+	        c++;
+	    }
+	    return c+1;
+	    // code here
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int h[n];
+        for (int i = 0; i < n; i++) {
+            cin >> h[i];
+        }
+        Solution ob;
+        auto ans = ob.countBuildings(h, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
