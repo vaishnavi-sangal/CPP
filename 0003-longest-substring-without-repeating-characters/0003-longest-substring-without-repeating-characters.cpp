ccfdc60ddf3ca11625d95a7538c899a5a3c94dfb
Vaishnavi Sangal <89699096+vaishnavi-sangal@users.noreply.github.com>
2023-09-06 01:56:25 -0700

    Time: 28 ms (31.98%), Space: 11 MB (20.29%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,37 +4,24 @@ public:
     int size=0;
     int n=s.size();
     int i=0,j=0;
-    if(s.size()==1)
-    return 1;
-    unordered_map<char,int>mp;
-    
-    while(j<n)
-    {
-   if(mp[s[j]]==0)
-   {
-       mp[s[j]]++;
-              size=max(size,(j-i+1));
-
-       j++;
-       cout<<"a";
-   }
-   else
-   {
-       cout<<" b";
-       if(mp[s[i]]>0)
-       {
-           mp[s[i]]--;
-           if(mp[s[i]]==0)
-           {
-               mp.erase(s[i]);
-           }
-       }
-       i++;
-       
-   }
-
-    }
-    return size;
+   unordered_map<char,int>mp;
+         while(j<n)
+         {
+             if(mp.find(s[j]) == mp.end())
+             {
+                 mp[s[j]]++;
+                 size=max(size,(j-i+1));
+                 j++;
+             }
+             else
+             {
+                 mp[s[i]]--;
+                 if(mp[s[i]]==0)
+                     mp.erase(s[i]);
+                 i++;
+             }
+         }
+         return size;
      }
 
 };
